c88dff304b7f5800faae0f99518fafe00494f21a
hayatoiwashita <iwashita511@gmail.com>
2020-11-13 17:30:47 +0900

    Added post method test code.

--- test/_index.js
+++ test/_index.js
@@ -15,13 +15,117 @@ describe("The express server", () => {
     it("get http-status-code 200", (done) => {
       // Setup
       // Exercise & Assert
-      request.get("/todos").end(function(err, res) {
+      request.get("/todos").end(function (err, res) {
         expect(res).to.have.status(200);
         expect(err).to.be.null;
         done();
       });
       // Teardown
     });
+
+    // it("get registered data", (done) => {
+    //   // Setup
+    //   const expected = [
+    //     {
+    //       "id": 1,
+    //       "user_id": 123,
+    //       "title": "study",
+    //       "content": "make todo API",
+    //       "status": "DONE",
+    //       "created": "1970-01-01T05:36:41.113Z",
+    //       "updated": "1970-01-01T05:36:41.113Z",
+    //       "createdAt": "2020-11-13T06:21:04.876Z",
+    //       "updatedAt": "2020-11-13T07:17:35.584Z"
+    //     },
+    //     {
+    //       "id": 3,
+    //       "user_id": 123,
+    //       "title": "study",
+    //       "content": "make todo API",
+    //       "status": "DONE",
+    //       "created": "1970-01-01T05:36:41.113Z",
+    //       "updated": "1970-01-01T05:36:41.113Z",
+    //       "createdAt": "2020-11-13T07:03:12.030Z",
+    //       "updatedAt": "2020-11-13T07:18:44.361Z"
+    //     },
+    //     {
+    //       "id": 4,
+    //       "user_id": 123,
+    //       "title": "shopping",
+    //       "content": "buy shoes",
+    //       "status": "PROGRESS",
+    //       "created": "1970-01-01T05:36:41.113Z",
+    //       "updated": "1970-01-01T05:36:41.113Z",
+    //       "createdAt": "2020-11-13T07:37:27.746Z",
+    //       "updatedAt": "2020-11-13T07:37:27.746Z"
+    //     }
+    //   ];
+
+    //   // Exercise & Assert
+    //   request.get("/todos").end(function(err, res) {
+    //     expect(res.body).to.deep.equal(expected);
+    //     expect(err).to.be.null;
+    //     done();
+    //   });
+    //   // Teardown
+    // });
   });
 
+  describe("GET /todos/:id", () => {
+    it("get http-status-code 200", (done) => {
+      // Setup
+      // Exercise & Assert
+      request.get("/todos/1").end(function (err, res) {
+        expect(res).to.have.status(200);
+        expect(err).to.be.null;
+        done();
+      });
+      // Teardown
+    });
+
+    // it("get the data of the specified id", (done) => {
+    //   // Setup
+    //   const expected = {
+    //     "id": 1,
+    //     "user_id": 123,
+    //     "title": "study",
+    //     "content": "make todo API",
+    //     "status": "DONE",
+    //     "created": "1970-01-01T05:36:41.113Z",
+    //     "updated": "1970-01-01T05:36:41.113Z",
+    //     "createdAt": "2020-11-13T06:21:04.876Z",
+    //     "updatedAt": "2020-11-13T07:17:35.584Z"
+    //   };
+
+    //   // Exercise & Assert
+    //   request.get("/todos/1").end(function(err, res) {
+    //     expect(res.body).to.deep.equal(expected);
+    //     expect(err).to.be.null;
+    //     done();
+    //   });
+    //   // Teardown
+    // });
+  });
+
+  describe("POST /todos", () => {
+    it("", (done) => {
+      // Setup
+      postData = {
+        "user_id": 123,
+        "title": "shopping",
+        "content": "buy shoes",
+        "status": "PROGRESS",
+        "created": 20201113,
+        "updated": 20201113
+      };
+
+      // Exercise & Assert
+      request.post("/todos").send(postData).end(function (err, res) {
+        expect(res).to.have.status(201);
+        expect(err).to.be.null;
+        done();
+      });
+      // Teardown
+    });
+  });
 });
