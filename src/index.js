b5738b7d64942fa18b0cb0419743fa411c3b188b
hayatoiwashita <iwashita511@gmail.com>
2020-11-13 21:40:27 +0900

    Add .env.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const { setupExpressServer } = require("./server");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.EXPRESS_PORT || 3000;
 const app = setupExpressServer();
 app.listen(PORT, () => {
   console.log("Server running on port", PORT)
