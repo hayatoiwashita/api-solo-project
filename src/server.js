ea10d8a39c676a1b983b300ab0fe7c557c850be4
hayatoiwashita <iwashita511@gmail.com>
2020-11-15 15:18:58 +0900

    Added cros module.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const cors = require('cors');
 const db = require("../models/index");
 
 const setupExpressServer = () => {
   const app = express();
 
-  app.use(express.json());
+  app.use(express.json(), cors({ origin: true, credentials: true }));
 
   app.get("/todos", function (req, res) {
     db.todos.findAll({}).then((todos) => {
